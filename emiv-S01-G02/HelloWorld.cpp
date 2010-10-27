5e4d355749d9dfe24cdf42e5a9b6802c8ba9f72c
hannesg <hag@informatik.uni-kiel.de>
2010-10-27 19:36:50 +0200

    initial commit

---
+++ emiv-S01-G02/HelloWorld.cpp
@@ -0,0 +1,22 @@
+/*
+Gruppe: 02
+Serie 01
+Matthias Boehm, 895778
+Hannes Georg, 850360
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello, world! " << endl;
+	cout << "What is your name? " << endl;
+	
+	string name;
+	cin >> name;
+	
+	cout << "Hello " << name << "! " << endl;
+	return 0;
+}
