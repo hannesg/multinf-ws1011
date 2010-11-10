32233191396c9d3dc02fa92ea892e1f86f8a1d5a
fliegenblues <fliegenblues@gmx.net>
2010-11-10 10:20:43 +0100

    Code aus den Konstruktoren in Funktion Init() ausgelagert

--- emiv-S03-G02/Graphics2D/PrimitiveLine.cpp
+++ emiv-S03-G02/Graphics2D/PrimitiveLine.cpp
@@ -9,20 +9,10 @@ Hannes Georg, 850360
 
 namespace Graphics2D {
 
-PrimitiveLine::PrimitiveLine() {
-	/* creates a line from origin to the origin, that is, an "empty" line */
-	points_.push_back(Coordinate(0, 0));
-	points_.push_back(Coordinate(0, 0));
-}
-
-PrimitiveLine::PrimitiveLine(const Coordinate &c1, const Coordinate &c2) {
+void PrimitiveLine::Init(const Coordinate &c1, const Coordinate &c2, const Color &c) {
 	points_.push_back(c1);
 	points_.push_back(c2);
-}
-
-PrimitiveLine::PrimitiveLine(int x1, int y1, int x2, int y2) {
-	points_.push_back(Coordinate(x1, y1));
-	points_.push_back(Coordinate(x2, y2));
+	SetColor(c);
 }
 
 void PrimitiveLine::Draw(ImageBase *img) const
--- emiv-S03-G02/Graphics2D/PrimitiveLine.hh
+++ emiv-S03-G02/Graphics2D/PrimitiveLine.hh
@@ -17,13 +17,14 @@ public:
 	/* constructors */
 
 	/* creates a line from origin to the origin, that is, an "empty" line */
-	PrimitiveLine();
+	PrimitiveLine() { Init(Coordinate(0, 0), Coordinate(0, 0), Color::black()); }
 
 	/* creates a line from c1 to c2 */
-	PrimitiveLine(const Coordinate &c1, const Coordinate &c2);
+	PrimitiveLine(const Coordinate &c1, const Coordinate &c2) { Init(c1, c2, Color::black()); }
+	PrimitiveLine(const Coordinate &c1, const Coordinate &c2, const Color &c) { Init(c1, c2, c); }
 
-	/* creates a line from (x1, y1) to (x2, y2)  */
-	PrimitiveLine(int x1, int y1, int x2, int y2);
+	/* creates a line from (x1, y1) to (x2, y2), for convenience  */
+	PrimitiveLine(int x1, int y1, int x2, int y2) { Init(Coordinate(x1, y1), Coordinate(x2, y2), Color::black()); }
 
 
 	/* Draw-Funktion */
@@ -35,6 +36,9 @@ protected:
 	virtual int GetNumberOfPointsAllowed() const { return 2; }
 
 private:
+	/* Initialization function, called from constructors */
+	void Init(const Coordinate &c1, const Coordinate &c2, const Color &c);
+
 	void DrawLine(ImageBase *img, const Coordinate &c1, const Coordinate &c2,const Color &color) const;
 
 	void DrawLineBresenham(ImageBase *img, const Coordinate &to,const Color &color,const char orthant, const Coordinate &offset) const;
