43ee29a82c8c55e00174f01a0c196d7215e44024
Matthias Boehm <fliegenblues@gmx.net>
2010-12-20 09:59:01 +0100

    Kommentare und Anfang von Freeman-Code

--- emiv-S09-G02/Graphics2D/Segmentation.cpp
+++ emiv-S09-G02/Graphics2D/Segmentation.cpp
@@ -30,10 +30,15 @@ Segmentation::~Segmentation() {
 }
 
 void Segmentation::AddHueSegment(const int label, const int minHue, const int maxHue, const int minSat) {
+	// Bild durchgehen
 	for(unsigned int x = 0; x < hsvImage_.GetWidth(); x++) {
 		for(unsigned int y = 0; y < hsvImage_.GetHeight(); y++) {
+			
+			// hue und saturation lesen
 			unsigned char pixel = hsvImage_.GetPixel(x, y, 0);
 			unsigned char sat = hsvImage_.GetPixel(x, y, 1);
+			
+			// Pruefen, ob in bounds
 			if(minHue <= pixel && pixel <= maxHue && minSat <= sat) {
 				labelImage_.SetPixel(x, y, 0, label);
 				labelImage_.SetPixel(x, y, 1, label);
@@ -45,6 +50,7 @@ void Segmentation::AddHueSegment(const int label, const int minHue, const int ma
 
 void Segmentation::ClosingOperation() {
 	Image tmp;
+	// einmal Dilation und Erosion durchfuehren
 	Filter::Rank3x3(labelImage_, tmp, 8);
 	Filter::Rank3x3(tmp, labelImage_, 0);
 }
@@ -55,13 +61,16 @@ void Segmentation::GetLabelImage(Image &labelImage) {
 
 int Segmentation::GetCenterAndArea(const int label, Coordinate &center, int &area) {
 	
+	// Initialisierung
 	center = Coordinate(0, 0);
 	area = 0;
 	
+	// Bild durchgehen
 	for(unsigned int i = 0; i < labelImage_.GetWidth(); i++) {
 		for(unsigned int j = 0; j < labelImage_.GetHeight(); j++) {
 			unsigned char pixel = labelImage_.GetPixel(i, j, 0);
-						
+			
+			// Wenn pixel im Label ist, Flaeche erhoehen und Punkt hinzufuegen
 			if(pixel == label) {
 				area++;
 				center += Coordinate(i, j);
@@ -69,6 +78,7 @@ int Segmentation::GetCenterAndArea(const int label, Coordinate &center, int &are
 		}
 	}
 	
+	// Mittelwert berechnen
 	center /= area;
 	
 	if(area != 0) {
@@ -76,6 +86,30 @@ int Segmentation::GetCenterAndArea(const int label, Coordinate &center, int &are
 	} else {
 		return 1;
 	}
+}
+
+int Segmentation::GetFreemanCode(const int label, Coordinate &firstPoint, std::vector<int> &freemanCode) {
+	
+	bool firstPointFound = false;
+	
+	// Ersten Punkt finden
+	for(unsigned int y = 0; y < labelImage_.GetHeight(); y++) {
+		for(unsigned int x = 0; x < labelImage_.GetWidth(); x++) {
+			if(labelImage_.GetPixel(x, y, 0) == label) {
+				// Erster Punkt gefunden -> abspeichern
+				firstPoint.SetX((int)x);
+				firstPoint.SetY((int)y);
+				firstPointFound = true;
+				break;
+			}
+		}
+	}
+	
+	if(!firstPointFound) { 
+		return 1;
+	}
+	
+	return 0;
 }
 
 }
